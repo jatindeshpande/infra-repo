278d1ad183ffaa148c67251efa6f7e840bf38c5a
jatindeshpande <jatinvd1997@gmail.com>
2026-04-29 12:18:31 +0530

    update data block for aws_eks_cluster

--- terraform.tf
+++ terraform.tf
@@ -32,6 +32,10 @@ provider "kubernetes" {
   token = data.aws_eks_cluster_auth.cluster.token
 }
 
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
+
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_name
 }
