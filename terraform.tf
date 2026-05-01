4ce98111bc71a88ecd0bdc9e5c867bd62e861bab
jatindeshpande <jatinvd1997@gmail.com>
2026-05-01 10:45:58 +0530

    update argocd

--- terraform.tf
+++ terraform.tf
@@ -32,6 +32,16 @@ provider "kubernetes" {
   token = data.aws_eks_cluster_auth.cluster.token
 }
 
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(
+      data.aws_eks_cluster.cluster.certificate_authority[0].data
+    )
+    token = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_name
   depends_on = [ module.eks ]
