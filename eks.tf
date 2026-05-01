7ac252c29a85b16e1207440c79aeb3fde59ee4b2
jatindeshpande <jatinvd1997@gmail.com>
2026-05-01 08:18:04 +0530

    update

--- eks.tf
+++ eks.tf
@@ -45,7 +45,7 @@ module "eks" {
 
   access_entries = {
     github_actions = {
-      principal_arn = "arn:aws:iam::${data.aws_caller_identity.current}:role/github-actions-role"
+      principal_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/github-actions-role"
 
       policy_associations = {
         admin_policy = {
--- terraform.tf
+++ terraform.tf
@@ -34,8 +34,10 @@ provider "kubernetes" {
 
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_name
+  depends_on = [ module.eks ]
 }
 
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_name
+  depends_on = [ module.eks ]
 }
